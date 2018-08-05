37d596b4262d7c7e30269e5fb89fce15f43261b1
Yun Chen <apkipa1@gmail.com>
2018-08-05 15:49:32 +0800

    Update code

--- WndProc.c
+++ WndProc.c
@@ -5,9 +5,12 @@
 #include <stdio.h>
 
 #include "Scintilla.h"
+#include "SciLexer.h"
 #include "libtcc.h"
 
 #include "public.h"
+
+#pragma comment(lib, "Imm32.lib")
 
 #define CODE_EDITOR_ID 101
 #define CONSOLE_ID	102	//NOTE: Console may be realized with AllocConsole()
@@ -40,7 +43,7 @@
 #define SYSMENU_RADIO_CODEPAGE_SC_GBK_TEXT L"简体中文 GBK (936)"
 
 #define ABOUT_TEXT L"关于"
-#define PROGRAM_INFO_TEXT L"Simple C Executer\n本程序使用的支持库:\nlibtcc 0.9.2.7\nScintilla 4.1.0"
+#define PROGRAM_INFO_TEXT L"Simple C Executer\n作者: apkipa\n本程序使用的支持库:\nlibtcc 0.9.2.7\nScintilla 4.1.0"
 
 #define STABILITY_WARNING_TITLE_TEXT L"稳定性警告"
 #define STABILITY_WARNING_TEXT L"取消勾选该选项可能会导致代码的潜在漏洞影响本程序，进而导致严重的损失!\n仍然要这么做吗?"
@@ -56,7 +59,7 @@
 #define SYSMENU_RADIO_CODEPAGE_SC_GBK_TEXT L"Simplified Chinese GBK (936)"
 
 #define ABOUT_TEXT L"About"
-#define PROGRAM_INFO_TEXT L"Simple C Executer\nLibraries used:\nlibtcc 0.9.2.7\nScintilla 4.1.0"
+#define PROGRAM_INFO_TEXT L"Simple C Executer\nAuthor: apkipa\nLibraries used:\nlibtcc 0.9.2.7\nScintilla 4.1.0"
 
 #define STABILITY_WARNING_TITLE_TEXT L"Stability Warning"
 #define STABILITY_WARNING_TEXT L"Unticking this option may cause the program affected by unstable code and lead to fatal errors!\nStill continue?"
@@ -67,6 +70,9 @@
 
 #define SYSMENU_RADIO_CODEPAGE_BEGIN SYSMENU_RADIO_CODEPAGE_UTF8
 #define SYSMENU_RADIO_CODEPAGE_END SYSMENU_RADIO_CODEPAGE_SC_GBK
+
+#define SCE_C_BEGIN SCE_C_DEFAULT
+#define SCE_C_END SCE_C_ESCAPESEQUENCE + 10
 
 HMENU menuCodePagePopup;
 
@@ -251,7 +257,30 @@ void WndProc_Create(HWND hwnd, LPCREATESTRUCT lpCreateStruct) {
 	HWND hwEditor;
 
 	hwEditor = CreateWindow(L"Scintilla", NULL, WS_CHILD | WS_VISIBLE, 0, 0, 100, 100, hwnd, (HMENU)CODE_EDITOR_ID, lpCreateStruct->hInstance, NULL);
-	SendMessage(hwEditor, SCI_STYLESETFONT, 0, (LPARAM)"Fixedsys");
+	SendMessage(hwEditor, SCI_SETLEXER, SCLEX_CPP, 0);
+	for (int i = SCE_C_BEGIN; i <= SCE_C_END; i++) {
+		SendMessage(hwEditor, SCI_STYLESETFONT, i, (LPARAM)"FixedSys");
+	}
+	SendMessage(hwEditor, SCI_SETTABWIDTH, 4, 0);
+	//SendMessage(hwEditor, SCI_STYLESETFONT, SCE_C_DEFAULT, (LPARAM)"Courier New");
+	SendMessage(hwEditor, SCI_STYLESETFORE, SCE_C_WORD, RGB(0, 0, 255));
+	//SendMessage(hwEditor, SCI_STYLESETFORE, SCE_C_PREPROCESSOR, RGB(0, 0, 255));
+
+	SendMessage(
+		hwEditor,
+		SCI_SETKEYWORDS,
+		0,
+		(LPARAM)
+		"int short float double signed unsigned char long void bool _Bool const static register auto wchar_t "
+		"int8_t int16_t int32_t int64_t uint8_t uint16_t uint32_t uint64_t intmax_t uintmax_t intptr_t uintptr_t "
+		"int_fast8_t int_fast16_t int_fast32_t int_fast64_t uint_fast8_t uint_fast16_t uint_fast32_t uint_fast64_t "
+		"int_least8_t int_least16_t int_least32_t int_least64_t uint_least8_t uint_least16_t uint_least32_t uint_least64_t "
+		"size_t ssize_t ptrdiff_t clock_t va_list struct enum union"
+	);
+
+	SendMessage(hwEditor, SCI_SETMARGINTYPEN, 0, SC_MARGIN_NUMBER);
+	SendMessage(hwEditor, SCI_SETMARGINWIDTHN, 0, SendMessage(hwEditor, SCI_TEXTWIDTH, STYLE_LINENUMBER, (LPARAM)"_1"));
+	//SendMessage(hwEditor, SCI_SETMARGINMASKN, 0, SC_MARK_ARROWS);
 	//SendMessage(hwEditor, SCI_SETCODEPAGE, 936, 0);
 
 	AddOperationBarButton(hwnd, lpCreateStruct->hInstance, OPERATION_BUTTON_RUN);
@@ -367,6 +396,24 @@ void WndProc_Size(HWND hwnd, int nResizingRequested, int nClientWidth, int nClie
 	RelocateOperationBarButton(hwnd);
 }
 
+void WndProc_Notify(HWND hwnd, int nIdCommon, NMHDR *pNmhdr) {
+	int nLinenumOriginalWidth, nLinenumCalculatedWidth;
+	char strBuffer[16];
+
+	if (pNmhdr->idFrom == CODE_EDITOR_ID) {
+		switch (pNmhdr->code) {
+		case SCN_MODIFIED:
+		case SCN_ZOOM:
+			nLinenumOriginalWidth = SendMessage(pNmhdr->hwndFrom, SCI_GETMARGINWIDTHN, 0, 0);
+			sprintf(strBuffer, "_%d", (int)SendMessage(pNmhdr->hwndFrom, SCI_GETLINECOUNT, 0, 0));
+			nLinenumCalculatedWidth = SendMessage(pNmhdr->hwndFrom, SCI_TEXTWIDTH, STYLE_LINENUMBER, (LPARAM)strBuffer);
+			if (nLinenumOriginalWidth != nLinenumCalculatedWidth)
+				SendMessage(pNmhdr->hwndFrom, SCI_SETMARGINWIDTHN, 0, (LPARAM)nLinenumCalculatedWidth);
+			break;
+		}
+	}
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	switch (msg) {
 	case WM_CREATE:
@@ -383,6 +430,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		break;
 	case WM_SIZE:
 		WndProc_Size(hwnd, (int)wParam, LOWORD(lParam), HIWORD(lParam));
+		break;
+	case WM_NOTIFY:
+		WndProc_Notify(hwnd, (int)wParam, (NMHDR*)lParam);
 		break;
 	}
 
--- public.h
+++ public.h
@@ -19,11 +19,13 @@ typedef struct tagSeparateMode_SharedData {
 
 #define UserSpace_FileStructureSize 32
 #define UserSpace_FileStructureReplace(_Stream) (		\
+	UserSpace__imp___iob == NULL ? (FILE*)(_Stream) :	\
 	(_Stream) == (*UserSpace__imp___iob) + 0 ? stdin :	\
 	(_Stream) == (*UserSpace__imp___iob) + 1 ? stdout :	\
 	(_Stream) == (*UserSpace__imp___iob) + 2 ? stderr :	\
 	(FILE*)(_Stream)									\
 )
+
 typedef struct tagUserSpace_FILE {
 	char placeholder[UserSpace_FileStructureSize];
 } UserSpace_FILE, *pUserSpace_FILE;
@@ -32,11 +34,18 @@ static UserSpace_FILE(*UserSpace__imp___iob)[3];
 static int UserSpace_getc(UserSpace_FILE *_Stream) {
 	return getc(UserSpace_FileStructureReplace(_Stream));
 }
-
 static int UserSpace_putc(int _Character, UserSpace_FILE *_Stream) {
 	return putc(_Character, UserSpace_FileStructureReplace(_Stream));
 }
-
+static int UserSpace_ungetc(int _Character, UserSpace_FILE *_Stream) {
+	return ungetc(_Character, UserSpace_FileStructureReplace(_Stream));
+}
+static int UserSpace_fgetc(UserSpace_FILE *_Stream) {
+	return fgetc(UserSpace_FileStructureReplace(_Stream));
+}
+static int UserSpace_fputc(int _Character, UserSpace_FILE *_Stream) {
+	return fputc(_Character, UserSpace_FileStructureReplace(_Stream));
+}
 static unsigned int UserSpace_sleep(unsigned int seconds) {
 	Sleep(1000 * seconds);
 	return 0;
@@ -45,23 +54,24 @@ static unsigned int UserSpace_sleep(unsigned int seconds) {
 #define TccLibFuncInject_ReplaceSameNameSymbol(pState, name) (tcc_add_symbol(pState, #name, name))
 #define TccLibFuncInject_ReplaceUserSpaceSymbol(pState, name) (tcc_add_symbol(pState, #name, UserSpace_ ## name))
 bool static inline TccLibFuncInject_Early(TCCState *pState) {
-	static FILE UserSpace__imp___iob[3];
-
-	UserSpace__imp___iob[0] = *stdin;
-	UserSpace__imp___iob[1] = *stdout;
-	UserSpace__imp___iob[2] = *stderr;
-
 	if (pState == NULL)
 		return false;
 
 	TccLibFuncInject_ReplaceSameNameSymbol(pState, printf);
 	TccLibFuncInject_ReplaceSameNameSymbol(pState, scanf);
+	TccLibFuncInject_ReplaceSameNameSymbol(pState, printf_s);
+	TccLibFuncInject_ReplaceSameNameSymbol(pState, scanf_s);
+	TccLibFuncInject_ReplaceSameNameSymbol(pState, gets_s);
+	TccLibFuncInject_ReplaceSameNameSymbol(pState, puts);
 	TccLibFuncInject_ReplaceSameNameSymbol(pState, getchar);
 	TccLibFuncInject_ReplaceSameNameSymbol(pState, putchar);
 
-	tcc_add_symbol(pState, "sleep", UserSpace_sleep);
-	tcc_add_symbol(pState, "getc", UserSpace_getc);
-	tcc_add_symbol(pState, "putc", UserSpace_putc);
+	TccLibFuncInject_ReplaceUserSpaceSymbol(pState, sleep);
+	TccLibFuncInject_ReplaceUserSpaceSymbol(pState, getc);
+	TccLibFuncInject_ReplaceUserSpaceSymbol(pState, putc);
+	TccLibFuncInject_ReplaceUserSpaceSymbol(pState, ungetc);
+	TccLibFuncInject_ReplaceUserSpaceSymbol(pState, fgetc);
+	TccLibFuncInject_ReplaceUserSpaceSymbol(pState, fputc);
 
 	return true;
 }
@@ -69,8 +79,34 @@ bool static inline TccLibFuncInject_Early(TCCState *pState) {
 bool static inline TccLibFuncInject_Late(TCCState *pState) {
 	UserSpace_FILE(**UserSpace_FileTemp)[3];
 
+	if (pState == NULL)
+		return false;
+
 	UserSpace_FileTemp = (UserSpace_FILE(**)[3])tcc_get_symbol(pState, "_imp___iob");
-	if (UserSpace_FileTemp != NULL)
-		UserSpace__imp___iob = *UserSpace_FileTemp;
+	UserSpace__imp___iob = UserSpace_FileTemp != NULL ? *UserSpace_FileTemp : NULL;
+
 	return true;
+}
+
+int static inline GetDigitCount(unsigned n) {
+	//Assuming unsigned is 32-bit wide
+	if (n < 10)
+		return 1;
+	if (n < 100)
+		return 2;
+	if (n < 1000)
+		return 3;
+	if (n < 10000)
+		return 4;
+	if (n < 100000)
+		return 5;
+	if (n < 1000000)
+		return 6;
+	if (n < 10000000)
+		return 7;
+	if (n < 100000000)
+		return 8;
+	if (n < 1000000000)
+		return 9;
+	return 10;	//Maximum digit count which unsigned 32-bit could reach
 }
